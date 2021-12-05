d9b4cb7d875b312b6911054322332e74b7e5e2be
Pascal Honegger <pascal.honegger@bluewin.ch>
2021-12-05 14:04:56 +0100

    feat(day5): use asSequence for better performance

--- src/Day05.kt
+++ src/Day05.kt
@@ -11,37 +11,36 @@ private fun String.toVector() = vectorRegex.matchEntire(this)?.let {
 } ?: error("Couldn't parse vector: $this")
 
 private fun Vector.part1Points() = when {
-    start == end -> listOf(start)
     start.x == end.x -> verticalPoints()
     start.y == end.y -> horizontalPoints()
-    else -> emptyList()
+    else -> emptySequence()
 }
 
 private fun Vector.part2Points() = when {
-    start == end -> listOf(start)
     start.x == end.x -> verticalPoints()
     start.y == end.y -> horizontalPoints()
     abs(start.x - end.x) == abs(start.y - end.y) -> diagonalPoints()
-    else -> emptyList()
+    else -> emptySequence()
 }
 
 private infix fun Int.coordinatesTo(end: Int) =
     IntProgression.fromClosedRange(this, end, if (this < end) 1 else -1)
 
 private fun Vector.horizontalPoints() =
-    (start.x coordinatesTo end.x).map { start.copy(x = it) }
+    (start.x coordinatesTo end.x).asSequence().map { start.copy(x = it) }
 
 private fun Vector.verticalPoints() =
-    (start.y coordinatesTo end.y).map { start.copy(y = it) }
+    (start.y coordinatesTo end.y).asSequence().map { start.copy(y = it) }
 
 private fun Vector.diagonalPoints() =
-    (start.x coordinatesTo end.x).mapIndexed { index, x ->
+    (start.x coordinatesTo end.x).asSequence().mapIndexed { index, x ->
         val y = if (start.y < end.y) start.y + index else start.y - index
         Point(x, y)
     }
 
 fun main() {
     fun part1(input: List<String>): Int = input
+        .asSequence()
         .map { it.toVector() }
         .flatMap { it.part1Points() }
         .groupingBy { it }
@@ -49,6 +48,7 @@ fun main() {
         .count { it.value >= 2 }
 
     fun part2(input: List<String>): Int = input
+        .asSequence()
         .map { it.toVector() }
         .flatMap { it.part2Points() }
         .groupingBy { it }
